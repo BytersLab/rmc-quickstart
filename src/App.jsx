7cf042cbb8c74d0e942818d903192c4ca538349c
Devcodino <devcodino@proton.me>
2024-11-25 19:00:45 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import "./App.css";
-import { Home } from "./routes/Home/home.route.jsx";
+import { Home } from "./routes/Home/Home.route.jsx";
 import { NotFound } from "./routes/NotFound/NotFound.route.jsx";
 
 export default function App() {
